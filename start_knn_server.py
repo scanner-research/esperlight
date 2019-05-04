deb80ce437d22ee835b58d19039e63f2c84e720b
Sumith Kulal <sumith1896@gmail.com>
2019-05-03 22:51:33 -0700

    fixes to server script

--- start_knn_server.py
+++ start_knn_server.py
@@ -1,52 +1,65 @@
-from flask import Flask
 import os
 import json
+import argparse
 
 import faiss
 import numpy as np
 
+from flask import Flask
 from flask import jsonify
 from flask import request
 
 app = Flask(__name__) 
-xb, video_dict, index = None, None, None
 
-@app.route('/setup', methods = ['POST'])
+## Arguments parser
+from ConfigParser import SafeConfigParser
+config = SafeConfigParser()
+config.read('knn_server_config.ini')
+video_dir = config.get('main', 'video_dir')
+minx = config.get('main', 'minx')
+miny = config.get('main', 'miny')
+score = config.get('main', 'score')
+
+## Store the params to be returned for `status`
+params = {"minx": minx, "miny": miny, "score": score}
+
+## Find the videos 
+video_list = []
+for file in os.listdir(video_dir):
+    if file.endswith(".mp4"):
+        video_list.append(file)
+
+## Prepare data for knn-search
+video_dict = []
+first = True
+for video in video_list[:3]:
+    curr_data = np.load(os.path.join(video_dir, video[:-4] + '_' + str(minx) + '_' + 
+        str(miny) + '_' + str(score) + '_' + 'embeddings.npy'), allow_pickle=True)
+    video_dict.append((video, len(curr_data)))
+    if first:
+        xb = curr_data
+        first = False
+    else:
+        xb = np.concatenate((xb, curr_data), axis=0)
+
+## Create faiss index
+d = len(xb[0])
+index = faiss.IndexFlatL2(d)
+index.add(xb)
+
+@app.route('/status', methods = ['POST'])
 def setup_knn():
-	global xb, video_dict, index
-	try:
-	    jsondata = request.get_json()
-	    data = json.loads(jsondata)
-	    video_dir = data['video_dir']
-	    minx = data['minx']
-	    miny = data['miny']
-	    score = data['score']
-	    
-	    video_list = []
-	    for file in os.listdir(video_dir):
-	        if file.endswith(".mp4"):
-	            video_list.append(file)
-	    
-	    video_dict = []
-	    first = True
-	    for video in video_list[:3]:
-	        curr_data = np.load(os.path.join(video_dir, video[:-4] + '_' + str(minx) + '_' + 
-	        	str(miny) + '_' + str(score) + '_' + 'embeddings.npy'), allow_pickle=True)
-	        video_dict.append((video, len(curr_data)))
-	        if first:
-	            xb = curr_data
-	            first = False
-	        else:
-	            xb = np.concatenate((xb, curr_data), axis=0)
-	    
-	    d = len(xb[0])
-	    index = faiss.IndexFlatL2(d)
-	    index.add(xb)
+    global video_dict, params
+    try:
+        videos = []
+        for x in video_dict:
+            videos.append(x[0])
+        result = {'success': True, 'videos': videos, 'minx': params["minx"], \
+                'miny': params["miny"], 'score': params["score"]}
+    except:
+        result = {'error': True}
 
-	except:
-		result = {'error': True}
-
-	return jsonify(result)
+    return jsonify(result)
 
 def convert_idx_to_vidinfo(idx, video_dict):
     curr_sum = 0
@@ -67,25 +80,28 @@ def convert_vidinfo_to_idx(video_name, bbox_id):
 
 @app.route('/knn', methods = ['POST'])
 def find_knn():
-	global xb, video_dict, index
-	try:
-	    jsondata = request.get_json()
-	    data = json.loads(jsondata)
-	    video_name = data['video_name']
-	    bbox_id = data['bbox_id']
-	    k = data['k']
-	    
-	    xq = np.asarray([xb[convert_vidinfo_to_idx(video_name, bbox_id)]])
-	    D, I = index.search(xq, k)     # actual search
-	    print(I)
-	    bboxes = []
-	    for idx in I[0]:
-	        bboxes.append(convert_idx(idx, video_dict))
-	    reesult = {'success': True. 'bboxes': bboxes} 
-	except:
-		result = {'error': True}
+    global xb, video_dict, index
+    try:
+        jsondata = request.get_json()
+        data = json.loads(jsondata)
+        video_name = data['video_name']
+        bbox_id = data['bbox_id']
+        k = data['k']
 
-	return jsonify(result)
+        query_idx = convert_vidinfo_to_idx(video_name, bbox_id)
+        xq = np.asarray([xb[query_idx]])
+
+        D, I = index.search(xq, k)
+        print(I)
+        bboxes = []
+        for idx in I[0]:
+            bboxes.append(convert_idx_to_vidinfo(idx, video_dict))
+        result = {'success': True, 'bboxes': bboxes} 
+    except:
+        result = {'error': True}
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
+
